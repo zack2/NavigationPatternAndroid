3459a7027c656b6697efe0e5fa068712f76bc851
Olivier Loukombo <olivierloukombo@gmail.com>
2019-03-21 17:34:41 +0100

    navigation for android jetpack component

---
+++ app/src/main/java/imbcomgo/com/navigationpatternandroid/DeuxiemeFragment.java
@@ -0,0 +1,39 @@
+package imbcomgo.com.navigationpatternandroid;
+
+
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DeuxiemeFragment extends Fragment {
+
+
+    public DeuxiemeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_deuxieme, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.btn_f2).setOnClickListener(Navigation.createNavigateOnClickListener(R.id.fragment_3));
+    }
+}
